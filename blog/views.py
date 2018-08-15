a16fc1971f3e3d379db0839cd394cdd5c13777fa
Brzozova <brzozowwwska@gmail.com>
2018-08-15 14:10:35 +0200

    Midified templates to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def post_list(request): #funkcja pobiera request
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
     #zwraca funkcję render, która złoży w całość szablon "....html"
